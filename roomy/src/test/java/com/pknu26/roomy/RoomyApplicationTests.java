548b8bf8f39c16aee52ef33de5f3360f59f1f3ba
00nahyeonKim <nahyeon000918@gmail.com>
2026-05-22 11:49:18 +0900

    개발 환경 세팅

---
+++ roomy/src/test/java/com/pknu26/roomy/RoomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pknu26.roomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
